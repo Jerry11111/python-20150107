521d63489577c056ef3882875fc54a678b8d1fed
Jerry11111 <xiayiguo89@gmail.com>
2016-01-07 19:01:48 -0800

    i

---
+++ python_instance.py
@@ -0,0 +1,11 @@
+import time
+import os
+print 'Hello World!'
+i = 1
+time.sleep(1);
+print type(i)
+def run():
+    #os.system('ping www.baidu.com')
+    os.system('ls')
+    print 'running'
+run()
