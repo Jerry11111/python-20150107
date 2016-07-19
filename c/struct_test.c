954f0f7a5b36c1c36e8e6616b4437fc55b1bf9bb
Jerry11111 <xiayiguo89@gmail.com>
2016-07-19 05:09:36 -0700

    v

--- c/struct_test.c
+++ c/struct_test.c
@@ -12,6 +12,15 @@ int main(int argc, char **argv)
 	struct{	\
 		struct type *sle_next;	\
 	}
+	#define SLIST_INSERT_HEAD(head, elm, field) do {	\
+		elm->field.sle_next = head->slh_first;	\
+		head->slh_first = elm;	\
+    } while(0)
+	#define SLIST_INSERT_AFTER(slistelm, elm, field) do {	\
+		(elm)->field.sle_next = (slistelm)->field.sle_next;	\
+		(slistelm)->field.sle_next = (elm);	\
+	} while(0)
+
 	struct test{
 		int data;
 		SLIST_ENTRY(test) next;
@@ -21,6 +30,11 @@ int main(int argc, char **argv)
 	//printf("%d %p\n", (&test_head)->slh_first->data, (&test_head)->slh_first->next);
 	//printf("%d\n", (&test_head)->slh_first->data);
 	struct test *p = malloc(sizeof(struct test));
-	printf("%d\n", p->data);
+	p->data = 1;
+	struct test *p2 = malloc(sizeof(struct test));
+	p2->data = 2;
+	//p->next.sle_next = p2;
+	SLIST_INSERT_AFTER(p, p2, next);
+	printf("%d %d\n", p->data, (p->next).sle_next->data);
 	return 0;
 }
