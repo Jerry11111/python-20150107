c3e7fb092a02c5277fa5ff5eec3d3f40a91bbf1a
Jerry11111 <xiayiguo89@gmail.com>
2016-07-20 04:40:12 -0700

    v

--- c/struct_test.c
+++ c/struct_test.c
@@ -20,6 +20,9 @@ int main(int argc, char **argv)
 		(elm)->field.sle_next = (slistelm)->field.sle_next;	\
 		(slistelm)->field.sle_next = (elm);	\
 	} while(0)
+	#define LIST_REMOVE_HEAD(head, field) do {	\
+		(head)->slh_first = (head)->slh_first.field->sle_next;	\
+	} while(0)
 	#define SLIST_FIRST(head) ((head)->slh_first)
 	#define SLIST_END(head) NULL
 	#define SLIST_NEXT(elm, field) ((elm)->field.sle_next)
@@ -28,25 +31,25 @@ int main(int argc, char **argv)
 			(var) != SLIST_END(head);	\
 			(var) = SLIST_NEXT(var, field))
 
-	struct test{
+	struct entry{
 		int data;
-		SLIST_ENTRY(test) next;
+		SLIST_ENTRY(entry) next;
 	};
-	SLIST_HEAD(,test) test_head;// 定义head
-	bzero(&test_head, sizeof(test_head));// head初始化
-	//printf("%d %p\n", (&test_head)->slh_first->data, (&test_head)->slh_first->next);
-	//printf("%d\n", (&test_head)->slh_first->data);
-	struct test *p = malloc(sizeof(struct test));
+	SLIST_HEAD(,entry) head;// 定义head
+	bzero(&head, sizeof(head));// head初始化
+	//printf("%d %p\n", (&head)->slh_first->data, (&head)->slh_first->next);
+	//printf("%d\n", (&head)->slh_first->data);
+	struct entry *p = malloc(sizeof(struct entry));
 	p->data = 1;
-	struct test *p2 = malloc(sizeof(struct test));
+	struct entry *p2 = malloc(sizeof(struct entry));
 	p2->data = 2;
 	//p->next.sle_next = p2;
-	SLIST_INSERT_HEAD(&test_head, p, next);
+	SLIST_INSERT_HEAD(&head, p, next);
 	SLIST_INSERT_AFTER(p, p2, next);
 	//printf("%d %d\n", p->data, (p->next).sle_next->data);
-	printf("%d %d %d\n", (&test_head)->slh_first->data, p->data, (p->next).sle_next->data);
-	struct test *v = NULL;
-	SLIST_FOREACH(v, &test_head, next)
+	printf("%d %d %d\n", (&head)->slh_first->data, p->data, (p->next).sle_next->data);
+	struct entry *v = NULL;
+	SLIST_FOREACH(v, &head, next)
 	{
 		printf("%d\n", v->data);
 	}
