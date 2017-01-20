e29f30b9289b642dd85ebc5fdeb4ded76af7940f
Jerry11111 <xiayiguo89@gmail.com>
2017-01-20 23:14:10 +0800

    v

--- net/test.py
+++ net/test.py
@@ -22,25 +22,30 @@ class Arp(object):
 	send_ip_u4 = ''
 	recv_mac_u6 = ''
 	recv_ip_u4 = ''
+	padding_u18 = '\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
 
 	def toBytes(self):
-		print(self.dest_mac_u6 + self.sour_mac_u6 + self.frame_type_u2 + self.hard_type_u2 + self.prot_type_u2 + self.mac_len_u1 + self.ip_len_u1 + self.op_u2 + self.send_mac_u6 + self.send_ip_u4 + self.recv_mac_u6 + self.recv_ip_u4)
-		return bytearray(self.dest_mac_u6 + self.sour_mac_u6 + self.frame_type_u2 + self.hard_type_u2 + self.prot_type_u2 + self.mac_len_u1 + self.ip_len_u1 + self.op_u2 + self.send_mac_u6 + self.send_ip_u4 + self.recv_mac_u6 + self.recv_ip_u4, 'iso8859-1')
+		return bytearray(self.dest_mac_u6 + self.sour_mac_u6 + self.frame_type_u2 + self.hard_type_u2 + self.prot_type_u2 + self.mac_len_u1 + self.ip_len_u1 + self.op_u2 + self.send_mac_u6 + self.send_ip_u4 + self.recv_mac_u6 + self.recv_ip_u4 + self.padding_u18, 'iso8859-1')
 
-arp = Arp() 
-arp.dest_mac_u6 = '\xff\xff\xff\xff\xff\xff';
-arp.sour_mac_u6 = '\x08\x00\x27\xbd\xf9\x51'
-arp.frame_type_u2 = '\x08\x06'
-arp.hard_type_u2 = '\x00\x01'
-arp.prot_type_u2 = '\x08\x00'
-arp.mac_len_u1 = '\x01'
-arp.ip_len_u1 = '\x01'
-arp.op_u2 = '\x00\x01'
-send_mac_u6 = '\x08\x00\x27\xbd\xf9\x51'
-send_ip_u4 = '\x0a\x00\x02\x0f'
-recv_mac_u6 = '\x00\x00\x00\x00\x00\x00'
-recv_ip_u4 = '\x0a\x00\x02\x02'
-arr = arp.toBytes()
-print(arr, len(arr))
-for a in arr:
-	print(hex(a), end = '')
+def do_request():
+	s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+	s.bind(("lo", 0))
+	arp = Arp() 
+	arp.dest_mac_u6 = '\xff\xff\xff\xff\xff\xff';
+	arp.sour_mac_u6 = '\x08\x00\x27\xbd\xf9\x51'
+	arp.frame_type_u2 = '\x08\x06'
+	arp.hard_type_u2 = '\x00\x01'
+	arp.prot_type_u2 = '\x08\x00'
+	arp.mac_len_u1 = '\x01'
+	arp.ip_len_u1 = '\x01'
+	arp.op_u2 = '\x00\x01'
+	arp.send_mac_u6 = '\x08\x00\x27\xbd\xf9\x51'
+	arp.send_ip_u4 = '\x0a\x00\x02\x0f'
+	arp.recv_mac_u6 = '\x00\x00\x00\x00\x00\x00'
+	arp.recv_ip_u4 = '\x0a\x00\x02\x02'
+	arr = arp.toBytes()
+	print(arr, len(arr))
+	for a in arr:
+		print(hex(a), end = '')
+	s.send(arr)
+do_request()
